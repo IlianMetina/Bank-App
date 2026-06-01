4ccda474bd3a865d9cc13f7fc9138374f80b645e
Ilian Metina <ilian.metina83@gmail.com>
2026-06-01 16:20:53 +0200

    1st push

--- ms-accounts/src/main/java/com/myapp/ms_accounts/service/CustomerService.java
+++ ms-accounts/src/main/java/com/myapp/ms_accounts/service/CustomerService.java
@@ -1,7 +1,9 @@
 package com.myapp.ms_accounts.service;
 
 import com.myapp.ms_accounts.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
